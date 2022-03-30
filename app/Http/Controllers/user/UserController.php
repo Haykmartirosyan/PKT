595827b060b897a24dc39d002e87deb827ec6e78
Hayk Martirosyan <haykmartirosyan21@gmail.com>
2022-03-30 23:09:19 +0400

    test deploy

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
      */
     public function login(Request $request)
     {
+
         try {
             DB::beginTransaction();
             $credentials = $request->only(["user_email", "user_pass"]);
